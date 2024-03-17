95591f8cb4543ea87bdd702e632e973a591d3724
SHsus1122 <tjgurtn1122@naver.com>
2024-03-17 22:55:40 +0900

    배경, 플레이어, 장애물 기본 설정

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float jumpForce = 10;    // 점프 강도
+    public float gravityModifier;   // 중력 강도
+    public bool isOnGround = true;  // 플레이어 착지 상태
+
+    private Rigidbody playerRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // transform 은 모든 게임 오브젝트가 가지기에 자동으로 액세스(접근)를 지원합니다.
+        // 하지만 Rigidbody 는 별도로 추가하는 것이기에 자동으로 지원하지 않습니다.
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;     // 중력 정보를 가져와서 원하는 곱하기로 원하는 중력 강도로 설정합니다.
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            // AddForce 는 Transform.Translate 와 역할이 비슷합니다.
+            // 하지만, Translate 와 다르게 이는 위치만 사용해서 무언가를 물리적으로 움직이는 것이 아니라
+            // 오브젝트에 힘을 적용할 수 있습니다. 그래서 아래처럼만 하면 player에 1000만큼의 힘을 위쪽방향으로 주는 것입니다.
+            // 그래서 이 상태로 시작하면 캐릭터가 위로 점프하듯이 올라간 다음 중력에 의해 떨어지게 됩니다.
+            //  - 첫 번째 인자 : 힘을 줄 방향과 정도
+            //  - 두 번째 인자 : 힘을 어떤식으로 주는지에 대한 설정(Impulse 의 경우 즉시 힘을 적용해서 더 빠르게 뛰는 것처럼 보입니다)
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    // 플레이어가 오브젝트와 충돌시에 대한 이벤트 함수
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float repeateRate = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // SpawnObstacle 함수를 시작하고 2초 뒤부터 2초마다 계속해서 호출
+        InvokeRepeating("SpawnObstacle", startDelay, repeateRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnObstacle ()
+    {
+        // 스폰 설정 : 스폰할 프리팹, 스폰할 좌표, 스폰할 인스턴스의 회전값
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+    }
+}
